e028cd5b3f498c70dd9dbb5e5e7527091bf42441
Your Name <you@example.com>
2023-04-22 19:24:56 +0545

    file uploade

--- src/pages/studentlist/index.jsx
+++ src/pages/studentlist/index.jsx
@@ -1,16 +1,36 @@
 import api from "@/pages/api/axios";
 import React, { useEffect, useState } from "react";
 import NavBar from "@/component/NavBar";
-import { Progress, Space ,Rate} from "antd";
-import Reports from "@/component/charts/Reports";
+import StudentDetails from "./StudentDetails";
 
 const index = () => {
-  const [student, setStudent] = useState([]);
+  const [students, setStudent] = useState([]);
+  const [countAttendance, setCountAttendance] = useState(0);
+  const [studentDetails, setStudentDetails] = useState(null);
+  const [assighnemtReview,setAssignmentReview] = useState(0);
+
+  const studetnDetilas = async (Studentid, id) => {
+    try {
+      const res = await api.get(`/attendance/reports/${Studentid}`);
+      const resAssignment = await api.get(`/teacher/assignment/reports/${Studentid}`);
+      if (res && resAssignment) {
+        const present = parseInt(res.data.present);
+        setCountAttendance(present);
+        setAssignmentReview(resAssignment?.data?.performance);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   const studentlist = async () => {
     try {
       const res = await api.get("/teacher/class");
       if (res) {
         setStudent(res.data);
+        // !! this is function
+        studetnDetilas(res.data[0].studentId.id);
+        // !! this is state function
+        setStudentDetails(res.data[0]);
       }
     } catch (err) {
       console.log(err);
@@ -21,15 +41,16 @@ const index = () => {
     studentlist();
   }, []);
 
+  useEffect(()=>{},[countAttendance])
+
   return (
     <div>
       <div className="flex">
         <div>
           <NavBar />
         </div>
-        <div className="ml-56 mt-5 px-5">
+        <div className="ml-52 mt-5 px-5">
           <h1 className="text-2xl font-bold">Student list</h1>
-
           <div className="flex gap-20 mt-5 ">
             <div>
               {/* ====== Table Section Start */}
@@ -38,40 +59,55 @@ const index = () => {
                   <div className="p-1.5 min-w-full inline-block align-middle">
                     <div className="overflow-hidden">
                       <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                        <thead>
+                        <thead className="bg-gray-800 w-full text-white">
                           <tr>
                             <th
                               scope="col"
-                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase"
+                              className="px-2 py-3  text-xs font-bold uppercase"
+                            >
+                              Roll No
+                            </th>
+                            <th
+                              scope="col"
+                              className="px-6 py-3 text-xs font-bold  uppercase"
                             >
                               Name
                             </th>
                             <th
                               scope="col"
-                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase"
+                              className="px-6 py-3  text-xs font-bold  uppercase"
                             >
                               Email
                             </th>
                             <th
                               scope="col"
-                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase"
+                              className="px-6 py-3  text-xs font-bold uppercase"
                             >
                               Phone
                             </th>
                             <th
                               scope="col"
-                              className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase"
+                              className="px-6 py-3  text-xs font-bold uppercase"
                             >
                               Class
                             </th>
                           </tr>
                         </thead>
                         <tbody>
-                          {student.map((student, id) => {
+                          {students.map((student, id) => {
                             return (
                               <>
                                 <tr className="odd:bg-white even:bg-gray-100 dark:odd:bg-slate-900 dark:even:bg-slate-800">
                                   <td className="px-6 cursor-pointer hover:underline hover:text-red-600 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                                    {id + 1}
+                                  </td>
+                                  <td
+                                    onClick={() => {
+                                      studetnDetilas(students[id]?.studentId?.id);
+                                      setStudentDetails(students[id]);
+                                    }}
+                                    className="px-6 cursor-pointer hover:underline hover:text-red-600 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200"
+                                  >
                                     {student?.studentId?.firstName} {""}{" "}
                                     {student?.studentId?.lastName}{" "}
                                   </td>
@@ -80,9 +116,6 @@ const index = () => {
                                   </td>
                                   <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
                                     {student?.phoneNumber}
-                                  </td>
-                                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
-                                    {student?.address}
                                   </td>
                                   <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
                                     {student?.address}
@@ -99,58 +132,7 @@ const index = () => {
               </div>
             </div>
             <div className="card border w-96  hover:shadow-none shadow-slate-50 shadow-md px-5 rounded-md">
-              <div className=" relative flex flex-col mx-auto  m-5">
-                <img
-                  className="max-h-20 w-full opacity-80 absolute top-0"
-                  style={{ zIndex: -1 }}
-                  src="https://unsplash.com/photos/iFPBRwZ4I-M/download?force=true&w=640"
-                  alt=""
-                />
-                <div className="profile w-full flex m-3 ml-4 text-white">
-                  <img
-                    className="w-28 h-28 p-1 bg-white rounded-full"
-                    src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb"
-                    alt=""
-                  />
-                  <div className="title mt-11 ml-3 font-bold flex flex-col">
-                    <div className="name break-words">Ricky</div>
-                    <div className="add font-semibold text-sm italic dark">
-                      Designer
-                    </div>
-                  </div>
-                </div>
-                <div className="buttons flex absolute bottom-0 font-bold right-0 text-xs text-gray-500 space-x-0 my-3.5 mr-3">
-                  <div className="add border rounded-l-2xl rounded-r-sm border-gray-300 p-1 px-4 cursor-pointer hover:bg-gray-700 hover:text-white">
-                    Roll NO : 6
-                  </div>
-                  <div className="add border rounded-r-2xl rounded-l-sm border-gray-300 p-1 px-4 cursor-pointer hover:bg-gray-700 hover:text-white">
-                    CSIT
-                  </div>
-                </div>
-              </div>
-              <div className="flex gap-5 item center">
-                <div>
-                <h3 className="font-bold text-lg font-mono">Attendance</h3>
-                <Space wrap>
-                  <Progress
-                    type="circle"
-                    percent={90}
-                    strokeColor={{
-                      "0%": "#108ee9",
-                      "100%": "#87d068",
-                    }}
-                  />
-                </Space>
-                </div>
-                <div className="px-3">
-                  <span className="text-green-500 font-bold font-serif">Assignment Submit</span>
-                  <Rate allowHalf defaultValue={2.5}/>
-                </div>
-              </div>
-              <div className="py-4">
-              <h3 className="font-bold text-lg font-mono">Studnet Performance</h3>
-                <Reports height={300} width={370}/>
-              </div>
+              <StudentDetails studentDetails={studentDetails} countAttendance={countAttendance} assighnemtReview={assighnemtReview}/>
             </div>
           </div>
         </div>
